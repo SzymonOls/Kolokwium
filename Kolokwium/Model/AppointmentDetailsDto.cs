82be91e5d9d39e8bb0d61f13596049ccd261b565
Szymon Olszewski <s30524@pjwstk.edu.pl>
2025-05-16 13:47:25 +0200

    Initial local commit

---
+++ Kolokwium/Model/AppointmentDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Kolokwium.Model;
+
+public class AppointmentDetailsDto
+{
+    public DateTime Date { get; set; }
+    public PatientDto Patient { get; set; }
+    public DoctorDto Doctor { get; set; }
+    public List<AppointmentServiceDto> Services { get; set; }
+}
---
+++ Kolokwium/Model/DoctorDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Kolokwium.Model;
+
+public class DoctorDto
+{
+    public int DoctorId { get; set; }
+    public string Pwz { get; set; }
+}
